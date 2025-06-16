cd3387f20bd791bcaee961a1fc37284586775484
Simon Wörpel <simon.woerpel@pm.me>
2025-06-16 12:29:41 +0200

    ✨ (tasks) Add openaleph task

--- ftm_analyze/tasks.py
+++ ftm_analyze/tasks.py
@@ -2,11 +2,15 @@ from openaleph_procrastinate.app import make_app
 from openaleph_procrastinate.model import DatasetJob
 from openaleph_procrastinate.tasks import task
 
+from ftm_analyze.logic import analyze_entities
+
 app = make_app(__loader__.name)
 ORIGIN = "ftm-analyze"
 
 
 @task(app=app)
 def analyze(job: DatasetJob) -> DatasetJob:
-    print("ANALYZE")
+    with job.get_writer() as bulk:
+        for entity in analyze_entities(job.get_entities()):
+            bulk.put(entity, origin=ORIGIN)
     return job
