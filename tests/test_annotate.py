429a3e85eb672e56e761bf8b08d5341437e928b6
Simon Wörpel <simon.woerpel@pm.me>
2025-10-24 14:05:55 +0200

    ✅ (tests/annotate) Fix to updated rigour symbols

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -20,13 +20,9 @@ def test_annotate(documents):
     assert not a.repl
     assert a.annotate("Mrs. Jane Doe") == "Mrs. Jane Doe"
     a = Annotation(value="Mrs. Jane Doe", props={TAG_NAME.name})
-    assert (
-        a.repl == "[Mrs. Jane Doe](LEG&Q12573029&Q12791967&Q1682564&Q37110043&Q5407747)"
-    )
+    assert a.repl == "[Mrs. Jane Doe](LEG&Q12573029&Q12791967&Q1682564&Q37110043)"
     a = Annotation(value="Mrs. Jane Doe", props={TAG_PERSON.name})
-    annotated = (
-        "[Mrs. Jane Doe](LEG&PER&Q12573029&Q12791967&Q1682564&Q37110043&Q5407747)"
-    )
+    annotated = "[Mrs. Jane Doe](LEG&PER&Q12573029&Q12791967&Q1682564&Q37110043)"
     assert a.repl == annotated
     assert annotated in a.annotate("lorem ipsum Mrs. Jane Doe dolor")
 
@@ -50,7 +46,6 @@ def test_annotate_symbols():
     DARC = "IDIO Daten Import Export GmbH"
     assert get_symbol_annotations(model["Person"], JANE) == {
         "Q1682564",
-        "Q5407747",
         "Q12791967",
         "Q37110043",
         "Q12573029",
